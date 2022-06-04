c2f6cfed8b678c9173538519f7fedca67f512278
Abhishel Sawan <sawanabhi157@gmail.com>
2022-06-05 03:36:06 +0700

    fix url

--- Frontend/src/utils/index.js
+++ Frontend/src/utils/index.js
@@ -1,4 +1,4 @@
 export const SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://capital-shopping-api.herokuapp.com/"
+    ? "https://capital-shopping-api.herokuapp.com"
     : "http://localhost:5000";
